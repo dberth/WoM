2b8a2e07fc90a9af3b7e7e8cd16a4312aa72ac7a
dberth <denis.berthod@gmail.com>
2014-07-07 19:30:30 +0200

    Further implementation of engine.

--- src/mahjong_engine/engine.ml
+++ src/mahjong_engine/engine.ml
@@ -36,8 +36,30 @@ let start_engine action_handler world events =
     | Break_wall_roll x when (1 < x && x <= 12) -> wait_for_deal
     | event -> raise (Irrelevent_event (event, "wait_for_break_roll"))))
 
-  and wait_for_deal = lazy (new_state (function _ -> wait_for_deal))
+  and wait_for_deal = lazy (new_state (function
+    | Deal -> wait_for_draw_in_wall
+    | event -> raise (Irrelevent_event (event, "wait_for_deal"))))
+
+  and wait_for_draw_in_wall = lazy (new_state (function
+    | Draw _ -> player_turn
+    | event -> raise (Irrelevent_event (event, "wait_for_draw_in_wall"))))
+
+  and player_turn = lazy (new_state (function
+    | Discard _ -> tile_discarded
+    | Mahjong _ -> mahjong_declared
+    | Concealed_kong _ -> kong_declared
+    | Small_kong _ -> wait_for_kong_robbing
+    | event -> raise (Irrelevent_event (event, "player_turn"))))
+
+  and tile_discarded = lazy (new_state (fun _ -> tile_discarded))
 (*TODO*)
+
+  and mahjong_declared = lazy (new_state (fun _ -> mahjong_declared))
+
+  and kong_declared = lazy (new_state (fun _ -> kong_declared))
+  (*TODO*)
+
+  and wait_for_kong_robbing = lazy (new_state (fun _ -> wait_for_kong_robbing))
 
   in
   run action_handler world game_start events
