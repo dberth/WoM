c1e51e70255a20208c9482b028c3fbc7b0fe33b3
dberth <denis.berthod@gmail.com>
2014-07-09 19:46:32 +0200

    Add all hooks.

--- src/mahjong_engine/engine.mli
+++ src/mahjong_engine/engine.mli
@@ -24,6 +24,51 @@ type event =
 exception Irrelevant_event of (event * string)
 
 val build_engine:
-    ?on_game_start_exit: (event, 'world) action ->
+
       ?on_wait_for_wall_breaker_roll_entry: (event, 'world) action ->
-	unit -> ('world -> event list -> 'world * (event, 'world) Fsm.state)
+	?on_wait_for_break_roll_entry: (event, 'world) action ->
+	  ?on_wait_for_deal_entry: (event, 'world) action ->
+	    ?on_wait_for_draw_in_wall_entry: (event, 'world) action ->
+	      ?on_player_turn_entry: (event, 'world) action ->
+		?on_tile_discarded_entry: (event, 'world) action ->
+		  ?on_td_1_no_action_2_entry: (event, 'world) action ->
+		    ?on_td_1_chow_2_entry: (event, 'world) action ->
+		      ?on_td_1_pong_2_entry: (event, 'world) action ->
+			?on_td_1_kong_2_entry: (event, 'world) action ->
+			  ?on_td_1_no_action_1_entry: (event, 'world) action ->
+			    ?on_td_2_pong_1_entry: (event, 'world) action ->
+			      ?on_td_2_kong_1_entry: (event, 'world) action ->
+				?on_td_1_chow_1_entry: (event, 'world) action ->
+				  ?on_td_1_pong_1_entry: (event, 'world) action ->
+				    ?on_td_1_kong_1_entry: (event, 'world) action ->
+				      ?on_mahjong_declared_entry: (event, 'world) action ->
+					?on_kong_declared_entry: (event, 'world) action ->
+					  ?on_wait_for_kong_robbing_entry: (event, 'world) action ->
+					    ?on_kr_2_entry: (event, 'world) action ->
+					      ?on_kr_1_entry: (event, 'world) action ->
+
+						?on_game_start_exit: (event, 'world) action ->
+						?on_wait_for_wall_breaker_roll_exit: (event, 'world) action ->
+						  ?on_wait_for_break_roll_exit: (event, 'world) action ->
+						    ?on_wait_for_deal_exit: (event, 'world) action ->
+						      ?on_wait_for_draw_in_wall_exit: (event, 'world) action ->
+							?on_player_turn_exit: (event, 'world) action ->
+							  ?on_tile_discarded_exit: (event, 'world) action ->
+							    ?on_td_1_no_action_2_exit: (event, 'world) action ->
+							      ?on_td_1_chow_2_exit: (event, 'world) action ->
+								?on_td_1_pong_2_exit: (event, 'world) action ->
+								  ?on_td_1_kong_2_exit: (event, 'world) action ->
+								    ?on_td_1_no_action_1_exit: (event, 'world) action ->
+								      ?on_td_2_pong_1_exit: (event, 'world) action ->
+									?on_td_2_kong_1_exit: (event, 'world) action ->
+									  ?on_td_1_chow_1_exit: (event, 'world) action ->
+									    ?on_td_1_pong_1_exit: (event, 'world) action ->
+									      ?on_td_1_kong_1_exit: (event, 'world) action ->
+										?on_mahjong_declared_exit: (event, 'world) action ->
+										  ?on_kong_declared_exit: (event, 'world) action ->
+										    ?on_wait_for_kong_robbing_exit: (event, 'world) action ->
+										      ?on_kr_2_exit: (event, 'world) action ->
+											?on_kr_1_exit: (event, 'world) action ->
+
+
+											  unit -> ('world -> event list -> 'world * (event, 'world) Fsm.state)
