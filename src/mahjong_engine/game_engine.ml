6f17d2aba4f33f4ff4a583f3702051cf9591c9d5
dberth <denis.berthod@gmail.com>
2014-07-10 20:00:29 +0200

    Prepare for wall breaking handling.

--- src/mahjong_engine/game_engine.ml
+++ src/mahjong_engine/game_engine.ml
@@ -7,23 +7,38 @@ type game =
   {
     tiles: tile array;
     wall_breaker_roll: int;
+    current_tile: int;
   }
 
 let init_game =
   {
     tiles = [||];
     wall_breaker_roll = 0;
+    current_tile = 0;
   }
 
 let init_tiles =
-  [| b1; b1; b1; b1; b1; b2; b2; b2; b2; b3; b3; b3; b3; b4; b4; b4; b5; b5; b5; b5; b6; b6; b6; b6; b7; b7; b7; b7; b7; b8; b8; b8; b8; b9; b9; b9; b9;
-     d1; d1; d1; d1; d1; d2; d2; d2; d2; d3; d3; d3; d3; d4; d4; d4; d5; d5; d5; d5; d6; d6; d6; d6; d7; d7; d7; d7; d7; d8; d8; d8; d8; d9; d9; d9; d9;
-     c1; c1; c1; c1; c1; c2; c2; c2; c2; c3; c3; c3; c3; c4; c4; c4; c5; c5; c5; c5; c6; c6; c6; c6; c7; c7; c7; c7; c7; c8; c8; c8; c8; c9; c9; c9; c9;
+  [| b1; b1; b1; b1; b2; b2; b2; b2; b3; b3; b3; b3;
+     b4; b4; b4; b4; b5; b5; b5; b5; b6; b6; b6; b6;
+     b7; b7; b7; b7; b8; b8; b8; b8; b9; b9; b9; b9;
+
+     d1; d1; d1; d1; d2; d2; d2; d2; d3; d3; d3; d3;
+     d4; d4; d4; d4; d5; d5; d5; d5; d6; d6; d6; d6;
+     d7; d7; d7; d7; d8; d8; d8; d8; d9; d9; d9; d9;
+
+     c1; c1; c1; c1; c2; c2; c2; c2; c3; c3; c3; c3;
+     c4; c4; c4; c4; c5; c5; c5; c5; c6; c6; c6; c6;
+     c7; c7; c7; c7; c8; c8; c8; c8; c9; c9; c9; c9;
+
      gd; gd; gd; gd; rd; rd; rd; rd; wd; wd; wd; wd;
-     ww; ww; ww; ww; nw; nw; nw; nw; nw; ew; ew; ew; ew; sw; sw; sw; sw
+
+     ww; ww; ww; ww; nw; nw; nw; nw;
+     ew; ew; ew; ew; sw; sw; sw; sw
   |]
 
-let random_game = Array.make (Array.length init_tiles) (None: tile_descr option)
+let nb_tiles = Array.length init_tiles
+
+let random_game = Array.make nb_tiles (None: tile_descr option)
 
 let init_tile_descrs = Array.map tile_descr_of_tile init_tiles
 
